c9be1f6aedf3e0519c4625e393f262d3584cf706
joy <putra.joyo@amarbank.co.id>
2020-02-18 16:49:05 +0700

    changes

--- library/angular/projects/components/src/lib/components.component.ts
+++ library/angular/projects/components/src/lib/components.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'lib-components',
   template: `
     <p>
-      components workss!
+      components worksss!
     </p>
   `,
   styles: []
