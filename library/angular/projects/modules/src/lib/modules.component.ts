64d980137bfc1dbac7b6553c0954062cab24efbc
joy <putra.joyo@amarbank.co.id>
2020-02-18 15:52:36 +0700

    intial commit

---
+++ library/angular/projects/modules/src/lib/modules.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-modules',
+  template: `
+    <p>
+      modules works!
+    </p>
+  `,
+  styles: []
+})
+export class ModulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ library/angular/projects/modules/src/lib/modules.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ModulesComponent } from './modules.component';
+
+
+
+@NgModule({
+  declarations: [ModulesComponent],
+  imports: [
+  ],
+  exports: [ModulesComponent]
+})
+export class ModulesModule { }
